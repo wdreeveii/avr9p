a1feb70ac164d05a1ccb5cc3901d19ebfc410898
Whitham D. Reeve II <thetawaves@gmail.com>
2011-11-15 05:46:40 -0900

    Fixed improperly USART_Send declaration in usart.h

--- usart.h
+++ usart.h
@@ -3,7 +3,7 @@
 #define _USART_H
 
 void USART_Init(void);
-void USART_Send(char port, char *p_out, unsigned short length);
+void USART_Send(char port, char *p_out, uint16_t length);
 
 #define DSEND(p, s) USART_Send(p, s, strlen(s))
 

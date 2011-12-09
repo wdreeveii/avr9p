1c8ac218dc56ac5ec7fe23ef185195c2dfcce095
Whitham D. Reeve II <thetawaves@gmail.com>
2011-12-09 06:57:30 -0900

    Mount tool nolonger needs a properly configured serial port before running mount..

--- hostapps/mount.c
+++ hostapps/mount.c
@@ -12,17 +12,24 @@ int main(int argc, char *argv[])
 	int err = 0;
 	char options[200];
 	char *args[] = {"/bin/mount", "-v", "-t", "9p", "-o", options, argv[1], argv[2], (char*)NULL};
+	
+	speed_t devicespeed = B57600;
+	struct termios serialmode;
+	struct termios confirmation;
+	
 	if (argc < 3)
 	{
 		printf("mnt device mountpoint\n");
 		return;
 	}
+	// open
 	fd = open(argv[1], O_RDWR);
 	if (fd < 0)
 	{
 		printf("open error\n");
 		return;
 	}
+	// flush
 	err = tcflush(fd, TCIOFLUSH);
 	if (err < 0)
 	{
@@ -30,7 +37,34 @@ int main(int argc, char *argv[])
 		close(fd);
 		return;
 	}
+	// set terminal modes
+	err = tcgetattr(fd, &serialmode);
+	if (err < 0)
+	{
+		printf("tcgetattr error\n");
+		close(fd);
+		return;
+	}
+
+	serialmode.c_iflag &= ~(IGNBRK | BRKINT | IGNPAR | PARMRK | INPCK | ISTRIP | INLCR | IGNCR | ICRNL | IXON | IXOFF | IUCLC | IXANY | IMAXBEL | IUTF8);
+	serialmode.c_oflag &= ~(OPOST | OLCUC | OCRNL | ONLCR | ONOCR | ONLRET | OFILL | OFDEL );
+	serialmode.c_lflag &= ~(ISIG | ICANON | IEXTEN | ECHO | ECHOE | ECHOK | ECHONL | NOFLSH | XCASE | TOSTOP | ECHOPRT | ECHOCTL | ECHOKE);
+	serialmode.c_cflag &= ~(CSIZE | PARENB | PARODD | HUPCL | CSTOPB | CRTSCTS);
+	serialmode.c_cflag |= (CS8 | CREAD | CLOCAL);
+	err = cfsetspeed(&serialmode, devicespeed);
+	if (err < 0)
+	{
+		printf("cfsetspeed error\n");
+		close(fd);
+		return;
+	}
+	err = tcsetattr(fd, TCSANOW, &serialmode);
+	if (err < 0)
+	{
+		printf("tcsetattr error \n");
+		close(fd);
+		return;
+	}
 	sprintf(options, "debug=0xffff,noextend,trans=fd,rfdno=%i,wfdno=%i", fd, fd);
-	printf("%s\n", options);
 	execv("/bin/mount", args);
 }
